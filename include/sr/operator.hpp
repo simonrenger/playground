32323767539e64c2812b9c7781b900f46b8adf7a
Simon Renger <simon.renger@gmail.com>
2020-05-11 17:22:07 +0200

    [+] added base structure

---
+++ include/sr/operator.hpp
@@ -0,0 +1,10 @@
+#ifndef SR_MAIN_HPP_INCLUDED
+#define SR_MAIN_HPP_INCLUDED
+
+namespace sr {
+
+	int test();
+
+} 
+
+#endif // SR_MAIN_HPP_INCLUDED
---
+++ src/sr/operator.cpp
@@ -0,0 +1,7 @@
+#include "sr/operator.hpp"
+
+namespace sr {
+	int test() {
+		return 0;
+	}
+}
